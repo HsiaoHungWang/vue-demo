25bd0c8fd60b5fb37cfe72a9731c71d47cd4931d
HsiaoHungWang <wang@ispan.com.tw>
2024-10-01 20:39:35 +0800

    attribute binding 練習

--- src/components/ThirdComponent.vue
+++ src/components/ThirdComponent.vue
@@ -5,13 +5,15 @@ import { ref } from 'vue';
     const week = ["日", "一", "二", "三", "四", "五", "六"]
     const author = ref({    
     books: [
-        'Vue 2 - Advanced Guide',
+    'Vue 2 - Advanced Guide',
         'Vue 3 - Basic Guide',
         'Vue 4 - The Mystery'
     ]
-})
-
-
+   })
+   
+   const id = ref(10);
+   const homeUrl = ref('https://www.ispan.com.tw');
+   const isDisabled = ref(true);
     const isLogin = ref(false);
     const getName = () => {
         return 'Mary'
@@ -30,6 +32,18 @@ import { ref } from 'vue';
       <!--錯誤寫法 <p>{{ i+=1 }}</p> -->
       <p>星期{{week[new Date().getDay()]}}</p>
       <p>出版了書籍：{{author.books.length > 0 ? '是':'否'}}</p>
+      <hr />
+      <!--屬性繫節-->
+      <p v-bind:data-id="id">ID：{{ id }}</p>
+      <p :data-id="id">ID：{{ id }}</p>
+      <p :id="id">ID：{{ id }}</p>
+      <p :id>ID：{{ id }}</p>
+      <a :href="homeUrl">ispan</a>
+      <!--<img src="/public/dog1.jpg" />--><!--不建議這樣寫-->
+      <!--public 資料夾中檔案的讀取，不用加上public資料夾-->
+      <img src="/dogs/dog1.jpg" />
+      <img src="/src/assets/cat1.jpg" />
+      <button :disabled="isDisabled">click</button>
     </div>
 </template>
 
