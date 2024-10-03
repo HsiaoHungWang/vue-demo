325771d9f61518b448d6b213837b229e8993e443
HsiaoHungWang <wang@ispan.com.tw>
2024-10-03 19:45:57 +0800

    watch() 語法介紹

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref, watch } from 'vue';
 import BMIComponent from './components/BMIComponent.vue';
 import ButtonCount from './components/ButtonCount.vue';
 import ComputedDemo from './components/ComputedDemo.vue';
@@ -6,11 +7,42 @@ import FirstComponent from './components/FirstComponent.vue';
 import SecondComponent from './components/SecondComponent.vue';
 import ThirdComponent from './components/ThirdComponent.vue';
 
+const formatTime = today => {
+    const hours = String(today.getHours()).padStart(2, '0') //03、14
+    const minutes = String(today.getMinutes()).padStart(2, '0')
+    const seconds = String(today.getSeconds()).padStart(2, '0')
+    return `${hours}:${minutes}:${seconds}`
+}
+
+const today = new Date()
+const clock = ref(formatTime(today))
+const clock1 = ref({
+    day: today.getDay(),
+    theTime: formatTime(today)
+})
+// console.log(clock.value)
+
+// watch(clock, (newTime, oldTime) => {
+//     console.log('new：', newTime)
+//     console.log('old：', oldTime)
+// }, { immediate: true, once: false })
+//immediate: true 表示組件載入就要執行 watch
+
+watch(clock1, (newTime, oldTime) => {
+    console.log('new：', newTime)
+    console.log('old：', oldTime)
+}, { deep: true })
+
+//每隔一秒鐘讀取現在的時間
+setInterval(() => {
+    clock.value = formatTime(new Date())
+    clock1.value.theTime = formatTime(new Date())
+}, 1000);
 
 </script>
 
 <template>
-    <h2>Vue Demo</h2>
+    <h2>Vue Demo：{{ clock }}{{ clock1.theTime }}</h2>
     <hr />
     <!-- 按下 Tab 鍵-->
     <!-- <FirstComponent></FirstComponent>
