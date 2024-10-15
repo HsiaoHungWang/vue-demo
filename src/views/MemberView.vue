46235e3650648bc752221ead97cb03232d2f7ff2
HsiaoHungWang <wang@ispan.com.tw>
2024-10-15 20:23:17 +0800

    格式修正

--- src/views/MemberView.vue
+++ src/views/MemberView.vue
@@ -17,14 +17,15 @@ import { useRoute } from 'vue-router';
     <div>
         <!--在 template 中取得路由參數的值-->
         <!--第一種寫法 $route.params.[參數名稱]-->
-<h2>Member Center {{ $route.params.id }}</h2>
-<p>會員編號 {{ id }}</p>
-<!--第二種寫法-->
-<!--
-1. 路由設定檔中加上 props:true
-2. 在script，透過defineProps 宣告參數
-3. 在 tempate 中 {{ id }}
--->
+        <h2>Member Center {{ $route.params.id }}</h2>
+
+        <!--第二種寫法-->
+        <!--
+        1. 路由設定檔中加上 props:true
+        2. 在script，透過defineProps 宣告參數
+        3. 在 tempate 中 {{ id }}
+        -->
+        <p>會員編號 {{ id }}</p>
     </div>
 </template>
 
