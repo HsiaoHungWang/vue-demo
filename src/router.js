7bd2a91617f6f1ec0fd7289c3cb8dee91bd2233e
HsiaoHungWang <wang@ispan.com.tw>
2024-10-15 19:51:09 +0800

    vue-router redirect、alias

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import AboutView from "./views/AboutView.vue";
 import ContactView from "./views/ContactView.vue";
 import Team1View from "./views/Team1View.vue";
 import Team2View from "./views/Team2View.vue";
+import MyComponent from "./components/MyComponent.vue";
 
 //路由設定 path 比對 URL，比對成功就載入對應的組件
 const routes =[
@@ -11,32 +12,34 @@ const routes =[
     //http://localhost:5173/  => 載入 HomeView Component
     path:'/', 
     component:HomeView,
-    name:'Home'
+    name:'Home',
+    alias:'/index'
 },
 {
     //http://localhost:5173/about  => 載入 AboutView Component
     path:'/about',
     component:AboutView,
     name:'About',
+    redirect:'/about/team1',
     children:[
       {
         //http://localhost:5173/about/team1
-        path:'/about/team1', 
+        path:'team1', 
         component:Team1View, 
         name:'Team1'
       },
       
       {
         //http://localhost:5173/about/team2
-        path:'/about/team2', 
+        path:'team2', 
         component:Team2View, 
         name:'Team2'
-      }  
+      },
     ]
 },
 {
     //http://localhost:5173/contact  => 載入 ContactView Component
-    path:'/contact',
+    path:'/contactAbc',
     component:ContactView,
     name:'Abc'
 }]
