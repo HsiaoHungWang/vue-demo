d282c88cfa29eb40825040ff5b6b88188970482a
HsiaoHungWang <wang@ispan.com.tw>
2024-10-17 19:17:20 +0800

    v-for 的應用

--- src/views/DirectiveView.vue
+++ src/views/DirectiveView.vue
@@ -13,6 +13,7 @@ import { ref } from 'vue';
      {"code":"106","name":"大安區"}
    ])
    const selectedOption = ref('106')
+   const user = ref({"name":"Jack","age":30,"email":"Jack@company.com"})
 </script>
 
 <template>
@@ -31,12 +32,24 @@ import { ref } from 'vue';
     <p>isLoggedIn 為 False </p>
     <button @click="isLoggedIn=!isLoggedIn">登入</button>
  </div>
- <!--v-for讀取陣列-->
+ <!--v-for 讀取陣列-->
  <select v-model="selectedOption">
    <!-- region = {"code":"100","name":"中正區"}-->
     <option v-for="(region,index) in regions" :value="region.code" :key="region.code">{{index}} {{region.name}}</option>    
  </select>
  <span>{{ selectedOption }}</span>
+ <!--v-for 讀取物件-->
+ <ul>
+   <!--{key:value}-->
+   <li v-for="(value, key) in user">{{key}}:{{ value }}</li>
+ </ul>
+
+ <!--v-for 產生數字範圍 -->
+
+  <ul class="pagination pagination-lg">   
+    <li class="page-item" v-for="i in 8"><a class="page-link" href="#">{{ i }}</a></li>
+  </ul>
+
     </div>
 </template>
 
