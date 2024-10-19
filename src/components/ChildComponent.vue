4bbe89fd2c3e43bb68e7abd78302f722cc9616b7
HsiaoHungWang <wang@ispan.com.tw>
2024-10-19 10:57:23 +0800

    父子組件間的資料傳遞做法

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,33 @@
+<script setup>
+//  宣告父組件傳進來的屬性
+    const props = defineProps({
+        message: String,
+        title: String
+    })
+    //宣告父組件傳進來的事件
+     const emit = defineEmits(["myEvent"])
+
+     const clickHandler = () =>{
+        //觸發父組件的自訂事件
+        //並將資料傳給父組件
+        emit("myEvent", "來自子組件的資料")
+     }
+    
+</script>
+
+<template>
+    <div>
+    <h2>子組件</h2>
+    <p :title="title">{{ message }} {{ title }}</p>
+    <button @click="clickHandler">click</button>
+    </div>
+   
+</template>
+
+<style lang="css" scoped>
+div{
+    width:400px;
+    height:300px;
+    background-color: silver;
+}
+</style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,10 +1,25 @@
 <script setup>
+import ChildComponent from '@/components/ChildComponent.vue';
+import { ref } from 'vue';
+
+const abcd = ref("xyz")
+const parentData = ref("")
+//data 會收到子組件傳過來的值
+const myHandler = data => {
+  alert(data)
+  parentData.value = data
+}
 
 </script>
 
 <template>
     <div>
-        <h2>Contact Page</h2>
+        <h2>Contact Page {{ parentData }}</h2>
+        <!--
+        message、title 是自訂屬性名稱，用來將資料傳遞給子組件
+        myEvent 是一個自訂事件名稱，目前不知如何觸發此事件
+        -->
+        <ChildComponent message="Hello World!!" :title="abcd" @myEvent="myHandler"></ChildComponent>
     </div>
 </template>
 
