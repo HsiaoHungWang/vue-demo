6b5b20fd8f1fe53ed3ef84fe3accbac418b3e7f5
HsiaoHungWang <wang@ispan.com.tw>
2024-10-24 21:39:14 +0800

    分頁功能

--- src/views/SpotsView.vue
+++ src/views/SpotsView.vue
@@ -1,32 +1,65 @@
 <script setup>
-import { ref } from 'vue';
-
+import PagingComponent from '@/components/PagingComponent.vue';
+import { ref, watchEffect } from 'vue';
+   
     //串接 API => Ajax fetch()
-    // fetch('http://127.0.0.1:8000/api/spots/')
+    // fetch('http://127.0.0.1:8000/api/spots/?categoryid=3&search=山&page=2&page_size=5&ordering=-spotid')
     //   .then(response=>response.json())
     //   .then(datas=>console.log(datas))
-    const SPOTS = ref([])
+
+    //資料的搜尋、分頁、排序
+    const ITEM = ref({
+        "categoryid":3,
+        "search":"",
+        "ordering":"-spotid",
+        "page":1,
+        "page_size":9
+    })
+
+    const SPOTS = ref([]) //接收API回傳的結果
     const BASE_URL = import.meta.env.VITE_APIURL
     const API_URL= `${BASE_URL}/spots/`
 
-    const loadSpots = async()=>{
-        const response  = await fetch(API_URL)
+     //?categoryid=3&search=山&page=2&page_size=5&ordering=-spotid
+    watchEffect(async()=>{
+        const params = new URLSearchParams({
+        "categoryid":ITEM.value.categoryid,
+        "search":ITEM.value.search,
+        "ordering":ITEM.value.ordering,
+        "page":ITEM.value.page,
+        "page_size":ITEM.value.page_size
+    })
+    // console.log(params.toString())
+       const URL_PARAMS = `${API_URL}?${params.toString()}`
+        const response  = await fetch(URL_PARAMS)
         const datas = await response.json() 
         console.log(datas)       
         SPOTS.value = datas
         console.log(SPOTS.value)
+
+
+
+    })
+
+    //page 會由子組件傳過來
+    //分頁功能
+    const pagingHandler = page =>{
+       ITEM.value.page = page
     }
 
-    loadSpots()
-</script>
+
+
+   
+   </script>
 
 <template>
+    <PagingComponent :totalPages="SPOTS.total_page" @goPaging="pagingHandler"></PagingComponent>
     <div class="row row-cols-1 row-cols-md-3 g-4">
-  <div v-for="{spotid, spottitle, spotdescription, spotimage, address} in SPOTS.results" class="col">
+  <div v-for="{spotid, spottitle, spotdescription, spotimage, address} in SPOTS.results" class="col" :key="spotid">
     <div class="card h-100">
       <img :src="spotimage" class="card-img-top" :alt="spottitle">
       <div class="card-body">
-        <h5 class="card-title">{{ spottitle }}</h5>
+        <h5 class="card-title">{{spotid}} {{ spottitle }}</h5>
         <p class="card-text">{{ spotdescription.length <= 100 ? spotdescription : spotdescription.substring(0,100) }}...</p>
       </div>
       <div class="card-footer">
