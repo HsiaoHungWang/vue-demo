6e3b10dfff9f46fab01ce795f0d1969d61cf189e
HsiaoHungWang <wang@ispan.com.tw>
2024-10-25 08:56:45 +0800

    串接 API，分頁功能更新

--- src/components/PagingComponent.vue
+++ src/components/PagingComponent.vue
@@ -1,25 +1,35 @@
 <script setup>
-    const props = defineProps({
-        totalPages:Number
-    })
-    const emit = defineEmits(['goPaging'])
+import { computed, onMounted } from 'vue';
 
-const clickHandler = page =>{
-    emit('goPaging', page)
+
+const props = defineProps({
+  totalPages: Number,
+  thePage: Number
+})
+const emit = defineEmits(['goPaging'])
+
+const clickHandler = page => {
+  emit('goPaging', page)
 }
+
+// 計算屬性來限制頁面數量
+const limitedPages = computed(() => {
+  return Array.from({ length: Math.min(props.totalPages, 10) }, (_, i) => i + 1);
+});
 </script>
 
 <template>
-    <nav aria-label="...">
-  <ul class="pagination pagination-lg">
-    <li @click="clickHandler(page)" class="page-item" v-for="(page, idx) in totalPages" :key="page">
-      <span class="page-link">{{ page }}</span>
-    </li>
-   
-  </ul>
-</nav>
+  <nav>
+    <ul class="pagination pagination-lg">
+      <li @click="clickHandler(page)" class="page-item" v-for="(page, idx) in limitedPages" :key="page">
+        <span :class="{ 'active': thePage === page, 'page-link': true }">{{ page }}</span>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <style lang="css" scoped>
-
+.page-item {
+  cursor: pointer
+}
 </style>
--- src/views/SpotsView.vue
+++ src/views/SpotsView.vue
@@ -1,76 +1,91 @@
 <script setup>
 import PagingComponent from '@/components/PagingComponent.vue';
 import { ref, watchEffect } from 'vue';
-   
-    //串接 API => Ajax fetch()
-    // fetch('http://127.0.0.1:8000/api/spots/?categoryid=3&search=山&page=2&page_size=5&ordering=-spotid')
-    //   .then(response=>response.json())
-    //   .then(datas=>console.log(datas))
 
-    //資料的搜尋、分頁、排序
-    const ITEM = ref({
-        "categoryid":3,
-        "search":"",
-        "ordering":"-spotid",
-        "page":1,
-        "page_size":9
-    })
+//串接 API => Ajax fetch()
+// fetch('http://127.0.0.1:8000/api/spots/?categoryid=3&search=山&page=2&page_size=5&ordering=-spotid')
+//   .then(response=>response.json())
+//   .then(datas=>console.log(datas))
 
-    const SPOTS = ref([]) //接收API回傳的結果
-    const BASE_URL = import.meta.env.VITE_APIURL
-    const API_URL= `${BASE_URL}/spots/`
+//資料的搜尋、分頁、排序
+const ITEM = ref({
+  "categoryid": 0,
+  "search": "",
+  "ordering": "spotid",
+  "page": 1,
+  "page_size": 20
+})
 
-     //?categoryid=3&search=山&page=2&page_size=5&ordering=-spotid
-    watchEffect(async()=>{
-        const params = new URLSearchParams({
-        "categoryid":ITEM.value.categoryid,
-        "search":ITEM.value.search,
-        "ordering":ITEM.value.ordering,
-        "page":ITEM.value.page,
-        "page_size":ITEM.value.page_size
-    })
-    // console.log(params.toString())
-       const URL_PARAMS = `${API_URL}?${params.toString()}`
-        const response  = await fetch(URL_PARAMS)
-        const datas = await response.json() 
-        console.log(datas)       
-        SPOTS.value = datas
-        console.log(SPOTS.value)
+//將ITEM資料轉成QueryString
+const toQueryString = params => {
+  return Object.keys(params)
+    .filter(key => params[key] !== 0 && params[key] !== "")
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    .join('&');
+}
+
+// const queryString = toQueryString(ITEM.value);
+// console.log(queryString);
+
+const SPOTS = ref([]) //接收API回傳的結果
+const BASE_URL = import.meta.env.VITE_APIURL
+const API_URL = `${BASE_URL}/spots/`
+
+//?categoryid=3&search=山&page=2&page_size=5&ordering=-spotid
+watchEffect(async () => {
+  // const params = new URLSearchParams({
+  //   "categoryid": ITEM.value.categoryid,
+  //   "search": ITEM.value.search,
+  //   "ordering": ITEM.value.ordering,
+  //   "page": ITEM.value.page,
+  //   "page_size": ITEM.value.page_size
+  // })
+  // console.log(params.toString())
+  // const URL_PARAMS = `${API_URL}?${params.toString()}`
+
+  const URL_PARAMS = `${API_URL}?${toQueryString(ITEM.value)}`
+  const response = await fetch(URL_PARAMS)
+  const datas = await response.json()
+  console.log(datas)
+  SPOTS.value = datas
+  console.log(SPOTS.value)
 
 
 
-    })
+})
 
-    //page 會由子組件傳過來
-    //分頁功能
-    const pagingHandler = page =>{
-       ITEM.value.page = page
-    }
+//page 會由子組件傳過來
+//分頁功能
+const pagingHandler = page => {
+  ITEM.value.page = page
+}
 
 
 
-   
-   </script>
+
+</script>
 
 <template>
-    <PagingComponent :totalPages="SPOTS.total_page" @goPaging="pagingHandler"></PagingComponent>
-    <div class="row row-cols-1 row-cols-md-3 g-4">
-  <div v-for="{spotid, spottitle, spotdescription, spotimage, address} in SPOTS.results" class="col" :key="spotid">
-    <div class="card h-100">
-      <img :src="spotimage" class="card-img-top" :alt="spottitle">
-      <div class="card-body">
-        <h5 class="card-title">{{spotid}} {{ spottitle }}</h5>
-        <p class="card-text">{{ spotdescription.length <= 100 ? spotdescription : spotdescription.substring(0,100) }}...</p>
-      </div>
-      <div class="card-footer">
-      <small class="text-body-secondary">{{address}}</small>
-    </div>
-    </div>
+  <div class="d-flex justify-content-center">
+    <PagingComponent @goPaging="pagingHandler" :thePage="SPOTS.current_page" :totalPages="SPOTS.total_page">
+    </PagingComponent>
   </div>
- 
-</div>
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+    <div v-for="{ spotid, spottitle, spotdescription, spotimage, address } in SPOTS.results" class="col" :key="spotid">
+      <div class="card h-100">
+        <img :src="spotimage" class="card-img-top" :alt="spottitle">
+        <div class="card-body">
+          <h5 class="card-title">{{ spotid }} {{ spottitle }}</h5>
+          <p class="card-text">{{ spotdescription.length <= 100 ? spotdescription : spotdescription.substring(0, 100)
+              }}...</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-body-secondary">{{ address }}</small>
+        </div>
+      </div>
+    </div>
+
+  </div>
 </template>
 
-<style lang="css" scoped>
-
-</style>
+<style lang="css" scoped></style>
